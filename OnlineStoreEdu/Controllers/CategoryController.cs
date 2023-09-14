52c840b737a0d1b126f1d0ae7777e4759b9dcb60
Vadim Afanasev <trim-agency@yandex.ru>
2023-09-14 22:39:57 +0700

    Создал представление для списка категорий

--- OnlineStoreEdu/Controllers/CategoryController.cs
+++ OnlineStoreEdu/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStoreEdu.Data;
+using OnlineStoreEdu.Models;
 
 namespace OnlineStoreEdu.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
         }
     }
 }
