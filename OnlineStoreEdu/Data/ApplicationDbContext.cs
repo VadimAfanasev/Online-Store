140f48833e60512345600ffb9d26b747268b495b
Vadim Afanasev <trim-agency@yandex.ru>
2023-09-15 21:02:32 +0700

    создал модель, контроллер и представление для Application Type

--- OnlineStoreEdu/Data/ApplicationDbContext.cs
+++ OnlineStoreEdu/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace OnlineStoreEdu.Data
             
         }
         public DbSet<Category> Category { get; set; }
+        public DbSet<ApplicationType> ApplicationType { get; set; }
     }
 }
---
+++ OnlineStoreEdu/Models/ApplicationType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStoreEdu.Models
+{
+    public class ApplicationType
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
