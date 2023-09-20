8a236013880e24f30e180be194ee7c8a976ee99a
Vadim Afanasev <trim-agency@yandex.ru>
2023-09-20 22:42:23 +0700

    Добавил метод Get для редактирования категории

--- OnlineStoreEdu/Controllers/CategoryController.cs
+++ OnlineStoreEdu/Controllers/CategoryController.cs
@@ -38,5 +38,21 @@ namespace OnlineStoreEdu.Controllers
             }
             return View(); 
 		}
+
+		//GET - Edit
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id ==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+			return View(obj);
+		}
 	}
 }
