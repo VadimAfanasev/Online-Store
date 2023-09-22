ff2bad3ae80908b668a499e12a46ffcac787df84
Vadim Afanasev <trim-agency@yandex.ru>
2023-09-22 20:30:20 +0700

    сделал ДЗ

--- OnlineStoreEdu/Controllers/ApplicationTypeController.cs
+++ OnlineStoreEdu/Controllers/ApplicationTypeController.cs
@@ -4,34 +4,99 @@ using OnlineStoreEdu.Models;
 
 namespace OnlineStoreEdu.Controllers
 {
-    public class ApplicationTypeController : Controller
-    {
-        private readonly ApplicationDbContext _db;
+	public class ApplicationTypeController : Controller
+	{
+		private readonly ApplicationDbContext _db;
 
-        public ApplicationTypeController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
+		public ApplicationTypeController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
 
-        public IActionResult Index()
-        {
-            IEnumerable<ApplicationType> objList = _db.ApplicationType;
-            return View(objList);
-        }
+		public IActionResult Index()
+		{
+			IEnumerable<ApplicationType> objList = _db.ApplicationType;
+			return View(objList);
+		}
 
-        //GET - Create
+		//GET - CREATE
 		public IActionResult Create()
 		{
 			return View();
 		}
 
-        //POST - Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+		//POST - CREATE
+		[HttpPost]
+		[ValidateAntiForgeryToken]
 		public IActionResult Create(ApplicationType obj)
 		{
-            _db.ApplicationType.Add(obj);
-            _db.SaveChanges();
+			if (ModelState.IsValid)
+			{
+				_db.ApplicationType.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		//GET - EDIT
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var obj = _db.ApplicationType.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			return View(obj);
+		}
+
+		//POST - EDIT
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(ApplicationType obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.ApplicationType.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		//GET - DELETE
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var obj = _db.ApplicationType.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			return View(obj);
+		}
+
+		//POST - DELETE
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePost(int? id)
+		{
+			var obj = _db.ApplicationType.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.ApplicationType.Remove(obj);
+			_db.SaveChanges();
 			return RedirectToAction("Index");
 		}
 	}
--- OnlineStoreEdu/Models/ApplicationType.cs
+++ OnlineStoreEdu/Models/ApplicationType.cs
@@ -7,7 +7,9 @@ namespace OnlineStoreEdu.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+
 
     }
 }
