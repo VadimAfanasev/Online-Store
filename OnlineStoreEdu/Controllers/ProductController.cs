41800186abc15f4550734d93cd92e26648b0f3f6
Vadim Afanasev <trim-agency@yandex.ru>
2023-09-23 18:55:58 +0700

    Реализовал контроллер создания и добавление товара

--- OnlineStoreEdu/Controllers/ProductController.cs
+++ OnlineStoreEdu/Controllers/ProductController.cs
@@ -24,16 +24,30 @@ namespace OnlineStoreEdu.Controllers
             return View(objList);
         }
 
-        //GET - CREATE
-		public IActionResult Create()
+        //GET - UPSERT
+		public IActionResult Upsert(int? id)
 		{
-			return View();
+            Product product = new Product();
+            if (id == null)
+            {
+                //this is for create
+                return View(product);
+            }
+            else
+            {
+                product = _db.Product.Find(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+			    return View(product);
+            }
 		}
 
-        //POST - CREATE
+        //POST - UPSERT
         [HttpPost]
         [ValidateAntiForgeryToken]
-		public IActionResult Create(Product obj)
+		public IActionResult Upsert(Product obj)
 		{
             if (ModelState.IsValid) 
             {
@@ -43,36 +57,6 @@ namespace OnlineStoreEdu.Controllers
             }
             return View(); 
 		}
-
-		//GET - EDIT
-		public IActionResult Edit(int? id)
-		{
-            if (id == null || id ==0)
-            {
-                return NotFound();
-            }
-            var obj = _db.Product.Find(id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-
-			return View(obj);
-		}
-
-        //POST - EDIT
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Product.Update(obj);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
 
         //GET - DELETE
         public IActionResult Delete(int? id)
